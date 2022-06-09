7ce0fd030c455951619533acb9c54d5108a0ef5a
matheus <mat.feijo2506@gmail.com>
2022-06-09 18:14:16 -0400

    feat separando numero

--- Lexico.js
+++ Lexico.js
@@ -2,26 +2,60 @@
 import fs from "fs";
 import { eachLine } from "line-reader";
 
+const numbers = ["1","2","3","4","5","6","7","8","9"];
+const operators = ['+','/','-','*','^','(',')'];
 
 
-
-eachLine('./texto.txt',(line,last)=>{
+function getDadosArquivoTexto(url){
     
-    var textLine = line;
 
-    for(let i=0;i<textLine.length;i++){
-        if(textLine[i] === ' '){
-            continue;
-        }else{
-            console.log(textLine[i]);
+
+    eachLine(url,(line,last)=>{
+
+        const tokens = [];
+        for(let i=0;i<line.length;i++){
+            
+            if(line[i] === ' '){
+                continue;
+            }
+    
+            if(numbers.includes(line[i])){
+
+                var flag = -1;
+                var position = i;
+                var numSelect = parseInt(line[i]);
+                
+                //Percorre a linha ate não encontrar mais numeros
+                while(flag !== 0){
+                    //verifica se o proximo item é um numero tambem e adiciona na variavel
+                    if(numbers.includes(line[position + 1])){
+                        numSelect = numSelect * 10 + parseInt(line[position + 1]);
+                        position++;
+                    }else{
+                        flag = 0;
+                    }
+                }
+                i= position;
+
+                if(line[i+1] === "."){
+                    
+                }
+                tokens.push(numSelect);
+            }
+
+            if(operators.includes(line[i])){
+                tokens.push(line[i]);
+                //console.log(line[i]);
+            }
         }
-        
-    }
+        //console.log(tokens);
+        console.log("----")
+    })
 
-    console.log(" - - -")
-})
+}
 
 
+getDadosArquivoTexto('./texto.txt');
 
 
 
