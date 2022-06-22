6beaa4a0c508db2eddab9a297c43db621226b226
matheus <mat.feijo2506@gmail.com>
2022-06-22 11:39:33 -0400

    refactor lexico

--- src/Analisador.js
+++ src/Analisador.js
@@ -1,377 +1,130 @@
 import { eachLine } from "line-reader";
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const operators = ['+', '-', '*', '/', '^'];
+const operators = ['+', '-', '*', '/', '^', '(', ')', '[', ']'];
 const acceptedStages = [5, 20, 14, 17];
 
-export class Lexico {
+export class Analisador {
     static lexico(url) {
         eachLine(url, (line, last) => {
             const tokens = [];
             var num = 0;
             var stage = 0;
             var divisor = 1;
+            var expoente;
 
             for (let i = 0; i < line.length; i++) {
                 if (line[i] === " " || line[i] === "\t" || line[i] === "\n") {
                     continue;
                 }
 
-                /**stages dentro do parenteses */
                 if (stage === 0) {
-                    if (line[i] === "(") {
+                    if (line[i] === 'e') {
+                        expoente = 'e';
                         stage = 1;
-                        tokens.push(line[i]);
-
                     } else if (numbers.includes(line[i])) {
-                        stage = 14;
-
                         num = parseInt(line[i]);
-
-                    } else if (line[i] === 'e') {
-                        stage = 17;
-                        tokens.push('exp');
-                        i = i + 2;
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    // stage = (
-                } else if (stage === 1) {
-                    if (numbers.includes(line[i])) {
-                        stage = 2;
-
-                        num = parseInt(line[i]);
-
-                    } else if (line[i] === "-") {
-                        stage = 12;
-                        tokens.push(line[i]);
-
-                    } else if (line[i] === "e") {
-                        stage = 6;
-                        i = i + 2;
-                        tokens.push('exp');
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 2) {
-                    if (numbers.includes(line[i])) {
-
-                        stage = 2;
-                        num = num * 10 + parseInt(line[i]);
-
-                    } else if (operators.includes(line[i])) {
                         stage = 3;
+                    } else if (operators.includes(line[i])) {
 
-                        tokens.push(num);
-                        num = 0;
-
+                        stage = 6;
                         tokens.push(line[i]);
+                    } else {
+                        stage === 100;
+                    }
 
-                    } else if (line[i] === ".") {
-                        stage = 10;
-
+                } else if (stage === 1) {
+                    if (line[i] === 'x') {
+                        expoente = expoente + 'x';
+                        stage = 2;
                     } else {
                         stage = 100;
                     }
 
-                    //stage = *,-,+, /
+                } else if (stage === 2) {
+                    if (line[i] === 'p') {
+                        expoente = expoente + 'p';
+                        tokens.push(expoente);
+                        expoente = '';
+                        stage = 0;
+                    } else {
+                        stage = 100;
+                    }
+
+                    //stage = numbers
                 } else if (stage === 3) {
                     if (numbers.includes(line[i])) {
-                        stage = 4;
-                        num = parseInt(line[i]);
-
-                    } else if (line[i] === "e") {
-                        stage = 6;
-                        i = i + 2;
-                        tokens.push('exp');
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 4) {
-                    if (numbers.includes(line[i])) {
-                        stage = 4;
-
                         num = num * 10 + parseInt(line[i]);
-
-                    } else if (line[i] === ".") {
-                        stage = 11;
-
-                    } else if (operators.includes(line[i])) {
                         stage = 3;
-
-                        tokens.push(num);
-                        num = 0;
-
-                        tokens.push(line[i]);
-
-                    } else if (line[i] === ')') {
-                        stage = 5;
-                        tokens.push(num);
-                        num = 0;
-
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = )
-                } else if (stage === 5) {
-                    if (operators.includes(line[i])) {
-                        stage = 21;
-                        tokens.push(line[i]);
-
-                    }
-
-                    //stage = exp
-                } else if (stage === 6) {
-                    if (line[i] === '[') {
-                        stage = 7;
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = [
-                } else if (stage === 7) {
-                    if (numbers.includes(line[i])) {
-                        stage = 8;
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 8) {
-                    if (numbers.includes(line[i])) {
-                        stage = 8;
-                    } else if (line[i] === ']') {
-                        stage = 9;
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = ]
-                } else if (stage === 9) {
-                    if (operators.includes(line[i])) {
-                        stage = 3;
-                    } else if (line[i] === ')') {
-                        stage = 5;
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = .
-                } else if (stage === 10) {
-                    if (numbers.includes(line[i])) {
-                        stage = 12;
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = .
-                } else if (stage === 11) {
-                    if (numbers.includes(line[i])) {
-                        stage = 13;
-
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 12) {
-                    if (numbers.includes(line[i])) {
-                        stage = 12;
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else if (operators.includes(line[i])) {
-                        stage = 3;
-
-                        tokens.push(num);
-                        num = 0;
-                        divisor = 1;
-
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 13) {
-                    if (numbers.includes(line[i])) {
-                        stage = 13;
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else if (operators.includes(line[i])) {
-                        stage = 3;
-
-                        tokens.push(num);
-                        num = 0;
-                        divisor = 1;
-
-                        tokens.push(line[i]);
-
-
-                    } else if (line[i] === ')') {
-                        stage = 5;
-
-                        tokens.push(num);
-                        num = 0;
-                        divisor = 1;
-
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    /**stages fora do parenteses */
-
-                    //stage = n 
-                } else if (stage === 14) {
-                    if (numbers.includes(line[i])) {
-                        stage = 14;
-
-                        num = num * 10 + parseInt(line[i]);
-
                     } else if (line[i] === '.') {
-                        stage = 15;
-
+                        stage = 4
                     } else if (operators.includes(line[i])) {
-                        stage = 21;
+                        stage = 6;
                         tokens.push(num);
                         num = 0;
-
                         tokens.push(line[i]);
-
-                    }
-
-                    //stage = .
-                } else if (stage === 15) {
-                    if (numbers.includes(line[i])) {
-                        stage = 16;
-
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 16) {
-                    if (numbers.includes(line[i])) {
-                        stage = 16;
-                        divisor = divisor * 10;
-                        num = parseInt(num) + parseFloat(line[i] / divisor);
-
-                    } else if (operators.includes(line[i])) {
-                        stage = 21;
-                        tokens.push(num);
-                        num = 0;
-                        divisor = 1;
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = exp
-                } else if (stage === 17) {
-                    if (line[i] === '[') {
-                        stage = 18;
-                        tokens.push(line[i]);
-                    }
-
-                    //stage = [
-                } else if (stage === 18) {
-                    if (numbers.includes(line[i])) {
-                        stage = 19;
-                        num = parseInt(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = n
-                } else if (stage === 19) {
-                    if (numbers.includes(line[i])) {
-                        stage = 19;
-                        num = num * 10 + parseInt(line[i]);
-
-                    } else if (line[i] === ']') {
-                        stage = 20;
-                        tokens.push(num);
-                        num = 0;
-
-                        tokens.push(line[i]);
-
-                    } else {
-                        stage = 100;
-                    }
-
-                    //stage = ]  
-                } else if (stage === 20) {
-                    if (operators.includes(line[i])) {
-                        stage = 21;
-                        tokens.push(line[i]);
-
-                    }
-
-                    //stage = *, + , - , / , ^  
-                } else if (stage === 21) {
-                    if (numbers.includes(line[i])) {
-                        stage = 14;
-                        num = parseInt(line[i]);
-
-                        if (line[i + 1] === undefined) {
-                            tokens.push(num);
-                            num = 0;
-                        }
-
 
                     } else if (line[i] === 'e') {
-                        stage = 17;
-                        i = i + 2;
-                        tokens.push('exp');
-
-                    } else if (line[i] === '(') {
                         stage = 1;
-                        tokens.push(line[i]);
+                        expoente = 'e';
+                        tokens.push(num);
+                        num = 0;
+                    }
 
+                    //stage = ' . '
+                } else if (stage === 4) {
+                    if (numbers.includes(line[i])) {
+                        stage = 5;
+                        divisor = divisor * 10;
+                        num = parseInt(num) + parseFloat(line[i] / divisor);
                     } else {
                         stage = 100;
                     }
+
+                } else if (stage === 5) {
+                    if (numbers.includes(line[i])) {
+                        stage = 5;
+                        divisor = divisor * 10;
+                        num = parseInt(num) + parseFloat(line[i] / divisor);
+                    } else if (operators.includes(line[i])) {
+                        stage = 6;
+                        tokens.push(num);
+                        num = 0;
+                        divisor = 1;
+                        tokens.push(line[i]);
+                    } else if (line[i] === 'e') {
+                        tokens.push(num);
+                        num = 0;
+                        divisor = 1;
+                        stage = 1;
+                        expoente = 'e';
+                    }
+
+                    //stage operators = () [] - + * / ^
+                } else if (stage === 6) {
+                    if (numbers.includes(line[i])) {
+                        stage = 3;
+                        num = parseInt(line[i]);
+
+                        i === line.length - 1 && tokens.push(line[i]);
+
+                    } else if (line[i] === 'e') {
+                        stage = 1;
+                        expoente = 'e';
+                    } else if (operators.includes(line[i])) {
+                        stage = 6;
+                        tokens.push(line[i]);
+
+                    }
+
                 }
             }
-
-            if (acceptedStages.includes(stage)) {
+            if (stage === 100) {
+                throw new Error('Sintaxe nao permitida')
+            } else {
+                //console.log(tokens);
                 this.sintatico(tokens);
             }
-
-            //console.log(tokens);
-            //console.log(line)
-            //console.log(stage);
         });
 
     }
@@ -456,6 +209,8 @@ export class Lexico {
 
                 }
             }
+
+            console.log(gramar)
         }
         console.log(casamento);
     }
