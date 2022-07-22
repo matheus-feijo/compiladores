f12f3a6ca5f586edf902c4ec2af21686705a5c09
matheus <mat.feijo2506@gmail.com>
2022-07-22 13:42:00 -0400

    feat comentario

--- src/Analisador.js
+++ src/Analisador.js
@@ -1,5 +1,6 @@
 import { eachLine } from "line-reader";
 
+//FAZER um NPM install
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const operators = ['+', '-', '*', '/', '^', '(', ')', '[', ']'];
 
